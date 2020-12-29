9775c4dd29eaf3103a9cfb096d7ef5a437d4b747
Ankitsharmasbsgroupoftechnology <76736942+Ankitsharmasbsgroupoftechnology@users.noreply.github.com>
2020-12-29 20:00:46 +0530

    SBS group of technology
    I feel very happy.

---
+++ c.js
@@ -0,0 +1,15 @@
+
+var num1 = 455;
+var num2 = 789;
+// String
+console.log("num1 + num2");
+
+var marks = {
+    ravi: 37,
+    ruchi: 43,
+    ankit: 99
+}
+    console.log(marks);
+var a = true;
+var b = false;
+console.log(a,b);
